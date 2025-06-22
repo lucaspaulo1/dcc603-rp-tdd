26772ecf8ccda20175f54838386a58ed52ecc014
Lucas Paulo <lucaspaulo1@hotmail.com>
2025-06-22 20:09:02 -0300

    (2)

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -2,10 +2,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class DollarTest {
-   @Test
-   public void testMultiplication() {
-      Dollar five = new Dollar(5);
-      five.times(2);
-      assertEquals(10, five.amount);
-   }
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
 }
